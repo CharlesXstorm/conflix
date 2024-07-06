c806e9472ed2b58eefc4b8547f85b659df058954
Charles Ukadike <charlesukadike44@gmail.com>
2024-07-06 05:25:33 +0100

    vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-undef */
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
-dotenv.config({path:"./.env"});
+dotenv.config({ path: "./.env" });
 
 // https://vitejs.dev/config/
 export default defineConfig({
